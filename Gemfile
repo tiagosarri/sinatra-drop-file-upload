573b6feedf74fd057c6be3df708fd2babcd5c4db
= <=>
2012-07-19 19:34:32 -0300

    Criando o projeto

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source :gemcutter
+
+gem "sinatra"
+gem "sinatra-static-assets"
+
+gem "json"
+gem "httparty"
+gem "thin"
---
+++ app.rb
@@ -0,0 +1,29 @@
+require "sinatra"
+
+enable :sessions
+set :raise_errors, false
+set :show_exceptions, false
+
+
+helpers do
+  def host
+    request.env['HTTP_HOST']
+  end
+
+  def scheme
+    request.scheme
+  end
+
+  def url_no_scheme(path = '')
+    "//#{host}#{path}"
+  end
+
+  def url(path = '')
+    "#{scheme}://#{host}#{path}"
+  end
+
+end
+
+get "/" do
+  erb :index
+end
